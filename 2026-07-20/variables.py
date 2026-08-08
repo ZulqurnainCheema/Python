2f8601ef02a589e3687e62deaea3096ad04be134
= <zulqurnaincheema45@gmail.com>
2026-08-08 14:45:28 +0500

    Co-authored-by: TABISH825 <TABISH825@users.noreply.github.com> Co-authored-by: chabdullahtariq159-sys <chabdullahtariq159-sys@users.noreply.github.com> Co-authored-by: husnaincheema529-png <husnaincheema529-png@users.noreply.github.com>

---
+++ 2026-07-20/variables.py
@@ -0,0 +1,15 @@
+name = "tabish ali"
+age = 21
+departement = "computer science"
+roll_number = 2308
+semester = 5
+cgpa = 3.8
+is_paid =  False
+
+if is_paid == True:
+    print(f"{name} has paid the fees.")
+    print("name:" + name)   
+    print(f'age: {age}')
+    print("department: " + departement)
+else : 
+    print("Student is not registered")
